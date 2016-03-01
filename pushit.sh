c54f4d7348cba2ee258b39226eb71111dd326e9e
James Campbell <james@jamescampbell.us>
2016-02-29 23:52:33 -0500

    style updates

--- pushit.sh
+++ pushit.sh
@@ -2,8 +2,6 @@
 cd ~/projects/hugositejc/
 git pull
 hugo
-cd public/js/
-rm *.js
 cd ~/projects/hugositejc/public
 cp -R * /usr/share/nginx/html
 echo "completed new publication of causes & effects"
