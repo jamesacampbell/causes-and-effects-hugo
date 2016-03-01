f2c07a57bdb27174777ccac834aac3d65650913b
James Campbell <james@jamescampbell.us>
2016-02-29 23:41:30 -0500

    optimized image

--- pushit.sh
+++ pushit.sh
@@ -2,6 +2,8 @@
 cd ~/projects/hugositejc/
 git pull
 hugo
-cd public
+cd public/js/
+rm *.js
+cd ~/projects/hugositejc/public
 cp -R * /usr/share/nginx/html
 echo "completed new publication of causes & effects"
