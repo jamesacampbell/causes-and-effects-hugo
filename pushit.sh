b52bfee434874154b9c030b03099b0648a35c652
James Campbell <james@jamescampbell.us>
2016-06-06 15:13:39 -0400

    changes

--- pushit.sh
+++ pushit.sh
@@ -4,5 +4,5 @@ git pull
 hugo
 cd ~/projects/hugositejc/public
 #cp -R * /home/user-data/www/default
-cp -R * /usr/share/nginx/html
+sudo cp -R * /usr/share/nginx/html
 echo "completed new publication of causes & effects"
