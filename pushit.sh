1be6a953dc1c6a8f16433c20a4117968ea1452e4
J C <james@jamescampbell.us>
2018-05-26 17:13:27 -0400

    Updated documentation
    Added explanation of each step.

--- pushit.sh
+++ pushit.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo "initializing update and publish script for causes and effects"
+# go to project folder
 cd ~/projects/hugositejc/
+# pull latest changes
 sudo -S -u jamesc -i git -C ~/projects/hugositejc/ pull
+# compile via hugo command
 hugo
+# go to resulting public folder of compiled data, should move to environmental variables to help automate later
 cd /home/jamesc/projects/hugositejc/public
+# copy all to root web folder
 sudo /bin/cp -R * /home/user-data/www/default
-#sudo /bin/cp -R * /usr/share/nginx/html
 echo "completed new publication of causes & effects"
